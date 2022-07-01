c9c588ffca70b13012ec3e5a64f522caaea5fe25
Cristian Nadal <xtian.nadal@gmail.com>
2022-07-01 18:04:12 +0200

    Images may be added to Item

--- app/src/shoppinglist/boundary/Add.js
+++ app/src/shoppinglist/boundary/Add.js
@@ -16,8 +16,8 @@ class Add extends BElement {
                     <div class="field">
                         <label class="label">Name</label>
                         <div class="control">
-                            <input class="input is-primary" type="text" required name="name" placeholder="Name" @keyup=${e=>
-                            this.onUserInput(e)} autofocus>
+                            <input class="input is-primary" required name="name" placeholder="Name" @keyup=${e=>
+                this.onUserInput(e)} autofocus>
                         </div>
                     </div>
                 </div>
@@ -25,13 +25,49 @@ class Add extends BElement {
                     <div class="field">
                         <label class="label">Quantity</label>
                         <div class="control">
-                            <input class="input is-primary" type="text" required name="quantity" placeholder="Quantity"
-                                @keyup=${e=> this.onUserInput(e)} value="1">
+                            <input class="input is-primary" required name="quantity" placeholder="Quantity" @keyup=${e=>
+                this.onUserInput(e)} value="1">
                         </div>
                     </div>
                 </div>
             </div>
         
+            <div class="columns">
+                <div class="column">
+                    <div class="field">
+                        <label class="label">Image</label>
+                        <div class="control">
+                            <div class="file">
+                                <label class="file-label">
+                                    <input class="file-input" type="file" name="image" accept="image/*" capture
+                                        @change="${e => this.onImageChosen(e)}">
+                                    <span class=" file-cta">
+                                        <span class="file-icon">
+                                            <i class="fa fa-upload"></i>
+                                        </span>
+                                        <span class="file-label">
+                                            Choose an image
+                                        </span>
+                                    </span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <p class="image is-128x128">
+                        ${Object.entries(this.state)
+                            .filter(([key, value]) => key === 'image')
+                            .map(([key, value]) =>
+                                html`
+                                <img src="data:image/*;base64, ${value}" alt="Item image" />
+                                `
+                            )
+                        }
+                    </p>
+                </div>
+            </div >
+
             <div class="columns">
                 <div class="column">
                     <div class="field">
@@ -42,16 +78,28 @@ class Add extends BElement {
                         </div>
                     </div>
                 </div>
-            </div>
-        </form>
-        `;
+            </div >
+        </form >
+    `;
     }
 
-    onUserInput({ target: { name, value, type } }) {
-        if (type === 'number') {
-            value = Number(value); // cast to number
-        }
+    onUserInput({ target: { name, value } }) {
         itemUpdated(name, value);
+    }
+
+    onImageChosen({ target: { name, files } }) {
+        // Get a reference to the file
+        const file = files[0];
+        // Encode the file using the FileReader API
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            // Use a regex to remove data url part
+            const imageBase64 = reader.result
+                .replace('data:', '')
+                .replace(/^.+,/, '');
+            itemUpdated(name, imageBase64);
+        };
+        reader.readAsDataURL(file);
     }
 
     newItem(event) {
--- app/src/shoppinglist/boundary/List.js
+++ app/src/shoppinglist/boundary/List.js
@@ -13,17 +13,55 @@ class List extends BElement {
     }
 
     view() {
+
+        const itemsByDate = new Map();
+        this.state.forEach((item) => {
+            const { createdAt } = item;
+            const date = new Date(createdAt).toLocaleDateString();
+            const items = itemsByDate.get(date);
+            if (!items) {
+                itemsByDate.set(date, [item]);
+            } else {
+                items.push(item);
+            }
+        });
+
         return html`
         <div class="content">
             <ol>
-                ${this.state.map(item =>
-                    html`
+                ${Array.from(itemsByDate.entries()).map(([date, items]) => {
+                    return html`
                         <li>
-                            <span>${new Date(item.createdAt).toLocaleDateString()} - ${item.name} (${item.quantity})</span>
-                            <button @click="${_ => deleteItem(item.id)}" class="button is-small is-danger is-light">delete</button>
+                            <span><h6>${date}</h6></span>
                         </li>
-                    `
-                )}
+
+                        ${items.map((item) => {
+                            return html`
+                            <li>
+                                <span>
+                                    ${item.name} (${item.quantity})
+                                    ${Object.entries(item)
+                                        .filter(([key, value]) => key === 'image' && value)
+                                        .map(([key, value]) =>
+                                        html`
+                                        <p >
+                                            <img src="data:image/*;base64, ${value}" alt="Item image" />
+                                        </p>
+                                        `
+                                        )
+                                    }
+                                </span>
+                                
+                                <button @click="${_ => deleteItem(item.id)}" class="button is-medium is-success is-light">
+                                    <span class="icon">
+                                        <i class="fa fa-check" aria-hidden="true"></i>
+                                    </span>
+                                </button>
+                            </li>            
+                            `;    
+                        })}
+                    `;
+                })}
             </ol>
         </div>
         <!-- <button @click="${_ => gotAllItems()}" class="button is-small is-success is-light">refresh</button> -->
